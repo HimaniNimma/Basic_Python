9a8d35072dcbcb56a487da1d69634a3b6beb3718
Himani Nimma <it1298himani@gmail.com>
2026-03-02 19:09:43 +0530

    Fix indentation and improve comment for clarity

--- 2nd largest.py
+++ 2nd largest.py
@@ -1,3 +1,5 @@
-H=list(map(int,input("Enter ele to the list").split()))
+#2nd highest number using list
+H=list(map(int,input("Enter element to the list").split()))
 H.sort()
 print("The 2nd largest ele is :",H[len(H)-2])
+
