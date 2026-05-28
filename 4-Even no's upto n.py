0363eb17378d70e18dbfeabcd2d11c308638401c
Himani Nimma <it1298himani@gmail.com>
2026-05-28 18:15:27 +0530

    Rename file to '4-Even no's upto n.py'

---
+++ 4-Even no's upto n.py
@@ -0,0 +1,6 @@
+#Print even numbers upto n
+n=int(input())
+for h in range (1,n+1): 
+    if(h%2==0):
+        print(h)
+
--- 4-EvenNoFrom(1-100).py
+++
@@ -1,5 +0,0 @@
-#Print 100 numbers
-for h in range (1,101): 
-    if(h%2==0):
-        print(h)
-
